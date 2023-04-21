4e5f76f55ec473ea24e954ff80de02ea04f27882
Mattis <mattis3858@yahoo.com.tw>
2023-04-21 15:51:54 +0800

    First commit

---
+++ src/components/ContactUs.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+                Contact
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <aside className="eigth columns footer-widgets">
+              <div className="widget">
+                <h4>Linked in :
+                  {resumeData.linkedinId}
+                </h4>
+                <h4>Skype :
+                  {resumeData.skypeid}
+                </h4>
+              </div>
+            </aside>
+          </div>
+        </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,152 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Mattis Chu 朱茂榛",
+  role: "Junior Front End Web Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription: "",
+  socialLinks: [
+    {
+      name: "github",
+      url: "https://github.com/Mattis3858",
+      className: "fa fa-github",
+    },
+    {
+      name: "FB",
+      url: "https://www.facebook.com/profile.php?id=100010560076903",
+      className: "fa fa-facebook",
+    },
+    {
+      name: "IG",
+      url: "https://www.facebook.com/profile.php?id=100010560076903",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "畢業於新竹高中，現於政治大學資訊管理學系就讀，對於網頁開發尤其於前端部分有熱忱。能夠迅速完成任務並且不怕困難",
+  address: "台北市文山區",
+  website: "https://",
+  education: [
+    {
+      UniversityName: "國立政治大學 ",
+      specialization: "資訊管理學系 ",
+      hobby: "政治大學羽球校隊 資管系羽球系隊隊長",
+    },
+  ],
+  work: [
+    {
+      Achievements: "iuiruv\
+        ",
+    },
+  ],
+  skillsDescription: "Skills & Abilities",
+  skills: [
+    {
+      skillid: 1,
+      skillname: "React.js / Javascript",
+    },
+    {
+      skillid: 3,
+      skillname: "Node.js / ExpressJS",
+    },
+    {
+      skillid: 4,
+      skillname: "MySQL / MongoDB",
+    },
+    {
+      skillid: 5,
+      skillname: "Python / Flask",
+    },
+    {
+      skillid: 6,
+      skillname: "Java",
+    },
+  ],
+  portfolio: [
+    {
+      name: "unitialproperties.co.za",
+      description: "Python/Flask",
+      imgurl: "images/portfolio/1.png",
+    },
+    {
+      name: "www.familytree365.com",
+      description: "Vue.js",
+      imgurl: "images/portfolio/2.png",
+    },
+    {
+      name: "turn2me.ie",
+      description: "Laravel",
+      imgurl: "images/portfolio/3.png",
+    },
+    {
+      name: "cryptocars.me",
+      description: "Blockchain",
+      imgurl: "images/portfolio/4.png",
+    },
+    {
+      name: "highkey.com",
+      description: "Shopify",
+      imgurl: "images/portfolio/5.png",
+    },
+    {
+      name: "www.eldorado.gg",
+      description: "Vue.js",
+      imgurl: "images/portfolio/6.png",
+    },
+    {
+      name: "www.rybitt.com",
+      description: "React.js",
+      imgurl: "images/portfolio/7.png",
+    },
+    {
+      name: "jomi.com",
+      description: "Next.js",
+      imgurl: "images/portfolio/8.png",
+    },
+    {
+      name: "kindergartenprepchildcare.com",
+      description: "Wordpress",
+      imgurl: "images/portfolio/9.png",
+    },
+  ],
+  testimonials: [
+    {
+      avatar: "1.jpg",
+      description:
+        "Very Good Communication, Excellent Work, Knows his stuff. \n I recommend it to everyone. Rapide and Professional.",
+      name: "Ashely Purcell",
+    },
+    {
+      avatar: "2.jpg",
+      description:
+        "His work is amazing, I got exactly what I was looking for, and he communicates all the time to make sure you get what you need.",
+      name: "Joseph Myers",
+    },
+    {
+      avatar: "3.jpg",
+      description:
+        "Victor really went all the way to turn our precise vision into reality. Really grateful for the wonderful work and phenomenal communication! We wouldn't hesitate to work with him again.",
+      name: "Daniel Ferguson",
+    },
+    {
+      avatar: "4.jpg",
+      description:
+        "Overall, I would strongly recommend Victor to anyone seeking high-quality work and a professional work ethic. His abilities and competence were obvious, and his dedication to the project's success was remarkable. ",
+      name: "Laurence JIN",
+    },
+    {
+      avatar: "5.jpg",
+      description:
+        "One of the things that most impressed me about him was his experience and the high quality of him work. His attention to detail and readiness to make adjustments to ensure that the result met my exact expectations impressed me.",
+      name: "Thomas Zavala",
+    },
+    {
+      avatar: "6.jpg",
+      description:
+        " Victor was responsive and simple to deal with throughout the process.  His kept me informed of him progress and was always accessible to answer any queries I had. His was clearly invested in the project's success, and his enthusiasm was contagious. ",
+      name: "Charles Strauss",
+    },
+  ],
+};
+
+export default resumeData;
