da42d7f472ef98c7683e9cd39fb38bf9aa85ad9d
Mattis <mattis3858@yahoo.com.tw>
2025-04-07 00:33:19 +0800

    調整自傳、achievements、skills、contactus email

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -12,16 +12,44 @@ function ContactUs(props) {
         <aside className="eigth columns footer-widgets">
           <div className="widget">
             <h4>
-              LinkedIn : <a href={resumeData.linkedIn}>朱茂榛</a>
+              LinkedIn :{" "}
+              <a
+                href={resumeData.linkedIn}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                朱茂榛
+              </a>
             </h4>
             <h4>
-              Github : <a href={resumeData.github}>Mattis3858</a>
+              Github :{" "}
+              <a
+                href={resumeData.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {resumeData.github}
+              </a>
             </h4>
             <h4>
-              Email : <a href={resumeData.email}>mustang3858@gmail.com</a>
+              Email :{" "}
+              <a
+                href={"mailto:" + resumeData.email}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {resumeData.email}
+              </a>
             </h4>
             <h4>
-              Instagram : <a href={resumeData.instagramId}>zhumaozhen</a>
+              Instagram :{" "}
+              <a
+                href={resumeData.instagramId}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                zhumaozhen
+              </a>
             </h4>
             <h4>Discord : {resumeData.discordId}</h4>
             <h4>Line : {resumeData.lineId}</h4>
