cdfd724d0cf3ff54b355cd661f87d891e3cc184e
Mattis <mattis3858@yahoo.com.tw>
2025-05-14 01:37:34 +0800

    修正 header navbar a 的 href 為有正確連結

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
             Show navigation
           </a>
-          <a className="mobile-btn" href="#" title="Hide navigation">
+          <a className="mobile-btn" href="#home" title="Hide navigation">
             Hide navigation
           </a>
           <ul id="nav" className="nav">
